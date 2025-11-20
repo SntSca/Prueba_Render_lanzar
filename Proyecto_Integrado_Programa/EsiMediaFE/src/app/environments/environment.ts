9d8dc77a630191bc6061d37294fd5c880a51e2f4
SntSca <Santiago.Sanchez12@alu.uclm.es>
2025-11-20 04:27:54 +0100

    Mensaje de commit

--- Proyecto_Integrado_Programa/EsiMediaFE/src/app/environments/environment.ts
+++ Proyecto_Integrado_Programa/EsiMediaFE/src/app/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
 
-  API_USERS_BASE: 'https://prueba-render-lanzar.onrender.com',
+  API_USERS_BASE: 'http://localhost:8081',
 
-  API_CONTENIDOS_BASE: 'https://prueba-render-lanzar-contenidos.onrender.com',
+  API_CONTENIDOS_BASE: 'http://localhost:8082',
 
-  API_BASE: 'https://prueba-render-lanzar-contenidos.onrender.com'
+  API_BASE: 'http://localhost:8082'
 };
