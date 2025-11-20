6f1af32f632478552d7b6a42e900e5f1e1deec58
Santiago Sánchez-Celemín Arévalo <146009509+SntSca@users.noreply.github.com>
2025-11-20 04:06:11 +0100

    Update API base URLs in production environment

--- Proyecto_Integrado_Programa/EsiMediaFE/src/app/environments/environment.prod.ts
+++ Proyecto_Integrado_Programa/EsiMediaFE/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_USERS_BASE: 'https://prueba-render-lanzar.onrender.com',
-  API_CONTENIDOS_BASE: 'https://esimediacontenidos.onrender.com',
-  API_BASE: 'https://esimediacontenidos.onrender.com'
+  API_CONTENIDOS_BASE: 'https://prueba-render-lanzar-contenidos.onrender.com',
+  API_BASE: 'https://prueba-render-lanzar-contenidos.onrender.com'
 };
